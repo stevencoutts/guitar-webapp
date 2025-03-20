a71ce2595b86b6c50cd63e0dbe23742af611f794
Steven Coutts <stevec@couttsnet.com>
2025-03-20 22:04:43 +0000

    Update app.py
    Import flask_mirgate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
